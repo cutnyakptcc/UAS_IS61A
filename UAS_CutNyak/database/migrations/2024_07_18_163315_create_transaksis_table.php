8ad4584456ea10c9d3670669ce4eb1d6fda11ad1
cutnyakptcc <cutnyakputroecendanalp3i@gmail.com>
2024-07-18 23:52:40 +0700

    membuat database

---
+++ UAS_CutNyak/database/migrations/2024_07_18_163315_create_transaksis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->string('nofak');
+            $table->string('nm_cos');
+            $table->date('tgl_tra');
+            $table->string('tagihan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
