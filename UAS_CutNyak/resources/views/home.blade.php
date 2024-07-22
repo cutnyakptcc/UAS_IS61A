e131a1a06b67cf71063a6aef6eda1908211f3682
cutnyakptcc <cutnyakputroecendanalp3i@gmail.com>
2024-07-22 20:08:40 +0700

    dashboard

--- UAS_CutNyak/resources/views/home.blade.php
+++ UAS_CutNyak/resources/views/home.blade.php
@@ -1,25 +1,11 @@
 @extends('layouts.master')
+@section('title','Dashboard')
+@section('judul','Dashboard')
+@section('subjudul','Data Keseluruhan')
+@section('nama','Friend Cellular')
+@section('pages','Home')
+@section('menu','Dashboard')
+@section('content')
 
 
 
-<!-- @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection -->
--- UAS_CutNyak/resources/views/layouts/master.blade.php
+++ UAS_CutNyak/resources/views/layouts/master.blade.php
@@ -683,9 +683,9 @@
               <h1 class="flex-grow-1 fs-3 fw-semibold my-2 my-sm-3">@yield('judul')</h1>
               <nav class="flex-shrink-0 my-2 my-sm-0 ms-sm-3" aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                  <li class="breadcrumb-item">Pages</li>
-                  <li class="breadcrumb-item">Generic</li>
-                  <li class="breadcrumb-item active" aria-current="page">Blank</li>
+                  <li class="breadcrumb-item">@yield('pages')</li>
+                  <li class="breadcrumb-item">Friend Cellular</li>
+                  <li class="breadcrumb-item active" aria-current="page">@yield('menu')</li>
                 </ol>
               </nav>
             </div>
